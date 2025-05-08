e1c4dfc3f017ed1c6635aad7038ba50dbf234901
WilliamPrakash <willprakash@gmail.com>
2025-05-08 19:15:02 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Client_Connect.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
