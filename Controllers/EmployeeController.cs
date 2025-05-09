525b5621d992c76a791e28463cfe3843fa882b90
WilliamPrakash <willprakash@gmail.com>
2025-05-09 10:12:54 -0400

    Employee index page and controller added

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Client_Connect.Data;
+using Client_Connect.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Client_Connect.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #region Views
+        public IActionResult Index()
+        {
+            List<Employee> employees = _context.Employees.ToList();
+            return View(employees);
+        }
+        #endregion
+
+
+
+    }
+}
